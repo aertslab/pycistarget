e158cd17b2e00528a7df1ed4a692348961fce4fb
Carmen Bravo <carmen.bravogonzalezblas@kuleuven.be>
2021-04-14 18:59:27 +0200

    Add tomtom

--- pyCistrome/motif_enrichment_homer.py
+++ pyCistrome/motif_enrichment_homer.py
@@ -5,7 +5,7 @@ import pyranges as pr # with pyfaidx
 import ray
 import os
 import subprocess
-from .utils import *
+#from .utils import *
 
 from IPython.display import HTML
 
@@ -13,15 +13,21 @@ def homer_results(homer_dict, name, results='known'):
     if results == 'known':
         file = os.path.join(homer_dict[name].outdir, 'knownResults.html')
     if results == 'denovo':
-<<<<<<< HEAD
         file = os.path.join(homer_dict[name].outdir, 'homerResults.html')
-=======
-        file = os.path.join(homer_dict[name].outdir, '/homerResults.html')
->>>>>>> remotes/origin/master
     inplace_change(file, 'width="505" height="50"', 'width="1010" height="200"')
     return HTML(file)
 
-def homer_find_motifs_genome(homer_path, region_sets, outdir, genome, size='given', mask=True, denovo=False, length='8,10,12', n_cpu=5):
+def homer_find_motifs_genome(homer_path: str,
+                             region_sets: Dict[str, pr.PyRanges],
+                             outdir: str,
+                             genome: str,
+                             size: str = 'given',
+                             mask: bool = True,
+                             denovo: bool = False,
+                             length: str = '8,10,12',
+                             n_cpu: int = 5,
+                             meme_path: str = None,
+                             meme_collection_path: str = None):
     # Create logger
     level    = logging.INFO
     format   = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
@@ -37,16 +43,10 @@ def homer_find_motifs_genome(homer_path, region_sets, outdir, genome, size='give
     if not os.path.exists(bed_dir):
         os.mkdir(bed_dir)
     # Create bed files for Homer
-<<<<<<< HEAD
     for key in region_sets.keys():
         bed_path = os.path.join(bed_dir, key+'.bed')
         region_sets[key].Name =  coord_to_region_names(region_sets[key])
         region_sets[key].to_bed(path=bed_path, keep=False, compression='infer', chain=False)
-=======
-    for key in pr_regions_dict.keys():
-        bed_path = os.path.join(bed_dir, key+'.bed')
-        pr_regions_dict[key].to_bed(path=bed_path, keep=False, compression='infer', chain=False)
->>>>>>> remotes/origin/master
         bed_paths[key] = bed_path
     # Run Homer
     ray.init(num_cpus=n_cpu)
@@ -58,13 +58,25 @@ def homer_find_motifs_genome(homer_path, region_sets, outdir, genome, size='give
                                 size,
                                 mask,
                                 denovo,
-                                length) for name in list(bed_paths.keys())])
+                                length, 
+                                meme_path,
+                                meme_collection_path) for name in list(bed_paths.keys())])
     ray.shutdown()
     homer_dict={list(bed_paths.keys())[i]: homer_dict[i] for i in range(len(homer_dict))}
     return homer_dict
 
 @ray.remote
-def homer_ray(homer_path, bed_path, name, outdir, genome, size='given', mask=True, denovo=False, length='8,10,12'):
+def homer_ray(homer_path: str,
+              bed_path: str,
+              name: str,
+              outdir: str,
+              genome: str,
+              size: str = 'given',
+              mask: bool = True,
+              denovo: bool = False,
+              length: str = '8,10,12',
+              meme_path: str = None,
+              meme_collection_path: str = None):
     # Create logger
     level    = logging.INFO
     format   = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
@@ -76,12 +88,23 @@ def homer_ray(homer_path, bed_path, name, outdir, genome, size='given', mask=Tru
         os.mkdir(outdir)
     
     log.info('Running '+ name)
-    Homer_res = Homer(homer_path, bed_path, name, outdir, genome, size, mask, denovo, length)
+    Homer_res = Homer(homer_path, bed_path, name, outdir, genome, size, mask, denovo, length, meme_path, meme_collection_path)
     log.info(name + ' done!')
     return Homer_res
 
 class Homer(): 
-    def __init__(self, homer_path, bed_path, name, outdir, genome, size='given', mask=True, denovo=False, length='8,10,12'):
+    def __init__(self,
+                 homer_path: str,
+                 bed_path: str,
+                 name: str,
+                 outdir: str,
+                 genome: str,
+                 size: str = 'given',
+                 mask: bool = True,
+                 denovo: bool = False,
+                 length: str = '8,10,12',
+                 meme_path: str = None,
+                 meme_collection_path: str = None):
         self.homer_path = homer_path
         self.bed_path = bed_path
         self.genome = genome
@@ -91,6 +114,8 @@ class Homer():
         self.mask = mask
         self.denovo = denovo
         self.name = name
+        self.meme_path = meme_path
+        self.meme_collection_path = meme_collection_path
         self.known_motifs = None
         self.denovo_motifs = None
         self.known_cistromes = None
@@ -130,6 +155,8 @@ class Homer():
                 self.denovo_motifs = self.load_denovo()
             except:
                 log.info('No de novo results found')
+                
+        self.add_motif_annotation_homer()
         
     def load_known(self):
         known = pd.read_csv(os.path.join(self.outdir, 'knownResults.txt'), sep='\t')
@@ -140,6 +167,89 @@ class Homer():
         denovo.iloc[:,0] = [x.split('More')[0] for x in denovo.iloc[:,0]]
         denovo.to_csv(os.path.join(self.outdir, 'homerResults.txt'), sep='\t', index=False)
         return denovo
+    
+    def add_motif_annotation_homer(self):
+        # Create logger
+        level    = logging.INFO
+        format   = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+        handlers = [logging.StreamHandler(stream=sys.stdout)]
+        logging.basicConfig(level = level, format = format, handlers = handlers)
+        log = logging.getLogger('pyCistrome')
+        
+        
+        if self.known_motifs is not None:
+            if self.known_motifs[0] != 0:
+                # Prepare cistarget annotation
+                if 'mm' in self.genome:
+                    species = 'mus_musculus'
+                if 'dm' in self.genome:
+                    species = 'drosophila_melanogaster'
+                if 'hg' in self.genome:
+                    species = 'homo_sapiens'
+                ctx_motif_annotation = load_motif_annotations(species)
+                homer_motifs = 'homer__' + self.known_motifs['Consensus'] + '_' + [x.split('(')[0] for x in self.known_motifs['Motif Name']]
+
+                motifs['MotifID'] = homer_motifs
+                homer_motifs = [x for x in homer_motifs if x in ctx_motif_annotation.index.tolist()]
+                ctx_motif_annotation = ctx_motif_annotation.loc[list(set(homer_motifs))].reset_index()
+
+                # Prepare homer annotation
+                homer_motif_annotation = pd.read_csv(os.path.abspath(os.path.join(os.path.dirname(self.homer_path), '..', 'motifs/extras/table.txt')),
+                                                    sep='\t', error_bad_lines=False).iloc[:,[1,11]].dropna()
+                # If not human, convert by homology
+                if species is not 'homo_sapiens':
+                    dataset = Dataset(name='hsapiens_gene_ensembl',
+                                  host='http://www.ensembl.org')
+                    if species is 'mus_musculus':
+                        biomart_query = 'mmusculus_homolog_associated_gene_name'
+                    if species is 'drosophila_melanogaster':
+                        biomart_query = 'dmelanogaster_homolog_associated_gene_name'
+
+                    human2specie = dataset.query(attributes=['external_gene_name', biomart_query])
+                    human2specie.index = human2specie['Gene name']
+                    # Check that the TF has homolog
+                    TF_names = [x for x in homer_motif_annotation.iloc[:,1].tolist() if x in human2specie.index.tolist()]
+                    human2specie = human2specie.loc[TF_names,:]
+                    human2specie.columns = ['Symbol', 'Homolog']
+                    df = pd.merge(homer_motif_annotation, human2specie, on='Symbol', how='left')
+                    homer_motif_annotation = df.iloc[:,[0,2]]
+                    homer_motif_annotation.columns = ['Name', 'Symbol']
+
+                # We first bind the cisTarget annotation
+                motifs = pd.merge(motifs, ctx_motif_annotation, on='MotifID', how='left')
+                # We now bind the Homer annotation
+                homer_motif_annotation.columns = ['Motif Name', 'Homer_annot']
+                motifs = pd.merge(motifs, homer_motif_annotation, on='Motif Name', how='left')
+                # If Homer_annot is not in Direct_annot we will add it
+                # Concatenate
+                motifs.Direct_annot = [str(motifs.Direct_annot.tolist()[x]) + ', ' + str(motifs.Homer_annot.tolist()[x]) 
+                                   if (str(motifs.Homer_annot.tolist()[x]) not in str(motifs.Direct_annot.tolist()[x]))
+                                   else motifs.Direct_annot.tolist()[x] for x in range(df.shape[0])]
+                motifs.Direct_annot = motifs.Direct_annot.replace('nan, ', '', regex=True)
+                motifs.Direct_annot = motifs.Direct_annot.replace(', nan', '', regex=True)
+                motifs.drop(['MotifID', 'Homer_annot'], axis=1)
+                self.known_motifs = motifs
+        
+        if self.denovo_motifs is not None:
+            if self.denovo_motifs.shape[0] != 0:
+                if self.meme_path is None:
+                    log.info('Parameter meme_path is not provided. Skipping annotation of de novo motifs')
+                else if self.path_to_meme_collection is None:
+                    log.info('Parameter meme_collection_path is not provided. Skipping annotation of de novo motifs')
+                else:
+                    # Find closest match for denovo motifs in cistarget database (as meme)
+                    log.info('Comparing de novo motifs with given motif collection with tomtom')
+                    homer_motif_paths = glob.glob(os.path.join(self.outdir, 'homerResults', 'motif*[0-9$].motif'))
+                    homer_motif_paths = [x for x in homer_motif_paths if 'similar' not in x] 
+                    tomtom_pd = pd.concat([tomtom(x, self.meme_path, self.meme_collection_path) for x in homer_motif_paths])
+                    ctx_motif_annotation = load_motif_annotations(species)
+                    homer_motifs = [x for x in tomtom_pd.iloc[:,1].tolist() if x in ctx_motif_annotation.index.tolist()]
+                    ctx_motif_annotation = ctx_motif_annotation.loc[list(set(homer_motifs))].reset_index()
+                    ctx_motif_annotation = ctx_motif_annotation.rename(columns={'MotifID': 'Best Match/Tomtom'})
+                    # Bind cisTarget annotation
+                    tomtom_pd = pd.merge(tomtom_pd, ctx_motif_annotation, on='Best Match/Tomtom', how='left')
+                    motifs = pd.merge(self.denovo_motifs, tomtom_pd, on='Best Match/Details', how='left')
+                    self.denovo_motifs = motifs
 
     def find_motif_hits(self, n_cpu=1):
         if self.known_motifs is not None:
@@ -171,50 +281,3 @@ class Homer():
             denovo_motif_hits = pd.read_csv(os.path.join(self.outdir, 'homerResults_motif_hits.bed'), sep='\t', header=None)
             denovo_motif_hits  = denovo_motif_hits.groupby(3)[0].apply(lambda g: g.values.tolist()).to_dict()
             self.denovo_motif_hits = {k:denovo_motif_hits[k] for k in denovo_motif_hits.keys() if not k[0].isdigit()}
-            
-    def add_motif_annotation_homer(self,
-                              species: str):
-        # Prepare cistarget annotation
-        ctx_motif_annotation = load_motif_annotations(species)
-        homer_motifs = 'homer__' + self.known_motifs['Consensus'] + '_' + [x.split('(')[0] for x in self.known_motifs['Motif Name']]
-
-        motifs['MotifID'] = homer_motifs
-        homer_motifs = [x for x in homer_motifs if x in ctx_motif_annotation.index.tolist()]
-        ctx_motif_annotation = ctx_motif_annotation.loc[list(set(homer_motifs))].reset_index()
-
-        # Prepare homer annotation
-        homer_motif_annotation = pd.read_csv('/data/leuven/software/biomed/haswell_centos7/2018a/software/HOMER/4.10.4-foss-2018a/motifs/extras/table.txt',
-                                            sep='\t', error_bad_lines=False).iloc[:,[1,11]].dropna()
-        # If not human, convert by homology
-        if species is not 'homo_sapiens':
-            dataset = Dataset(name='hsapiens_gene_ensembl',
-                          host='http://www.ensembl.org')
-            if species is 'mus_musculus':
-                biomart_query = 'mmusculus_homolog_associated_gene_name'
-            if species is 'drosophila_melanogaster':
-                biomart_query = 'dmelanogaster_homolog_associated_gene_name'
-
-            human2specie = dataset.query(attributes=['external_gene_name', biomart_query])
-            human2specie.index = human2specie['Gene name']
-            # Check that the TF has homolog
-            TF_names = [x for x in homer_motif_annotation.iloc[:,1].tolist() if x in human2specie.index.tolist()]
-            human2specie = human2specie.loc[TF_names,:]
-            human2specie.columns = ['Symbol', 'Homolog']
-            df = pd.merge(homer_motif_annotation, human2specie, on='Symbol', how='left')
-            homer_motif_annotation = df.iloc[:,[0,2]]
-            homer_motif_annotation.columns = ['Name', 'Symbol']
-
-        # We first bind the cisTarget annotation
-        motifs = pd.merge(motifs, ctx_motif_annotation, on='MotifID', how='left')
-        # We now bind the Homer annotation
-        homer_motif_annotation.columns = ['Motif Name', 'Homer_annot']
-        motifs = pd.merge(motifs, homer_motif_annotation, on='Motif Name', how='left')
-        # If Homer_annot is not in Direct_annot we will add it
-        # Concatenate
-        motifs.Direct_annot = [str(motifs.Direct_annot.tolist()[x]) + ', ' + str(motifs.Homer_annot.tolist()[x]) 
-                           if (str(motifs.Homer_annot.tolist()[x]) not in str(motifs.Direct_annot.tolist()[x]))
-                           else motifs.Direct_annot.tolist()[x] for x in range(df.shape[0])]
-        motifs.Direct_annot = motifs.Direct_annot.replace('nan, ', '', regex=True)
-        motifs.Direct_annot = motifs.Direct_annot.replace(', nan', '', regex=True)
-        motifs.drop(['MotifID', 'Homer_annot'], axis=1)
-        self.known_motifs = motifs
