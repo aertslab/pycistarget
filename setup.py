3a83cf9750f01d6090c20f60c8ff02536a0489af
SeppeDeWinter <seppe.dewinter@hotmail.com>
2023-08-09 09:21:18 +0000

    Restructure package layout

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
-from setuptools import setup, find_packages
+import setuptools
 
 def read_requirements(fname):
     with open(fname, 'r', encoding='utf-8') as file:
         return [line.rstrip() for line in file]
 
 
-setup(
+setuptools.setup(
      name='pycistarget',
-     use_scm_version=True,
-     setup_requires=['setuptools_scm'],
-     packages=find_packages(),
-     include_dirs=["."],
+     version="1.0a1",
+     packages=setuptools.find_packages(where='src'),
+     package_dir={'': 'src'},
      install_requires=read_requirements('requirements.txt'),
      author="Carmen Bravo, Seppe de Winter",
      author_email="carmen.bravogonzalezblas@kuleuven.be, seppe.dewinter@kuleuven.be",
@@ -22,5 +21,6 @@ setup(
          "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
      ],
+      entry_points={"console_scripts": ["pycistarget = pycistarget.cli.pycistarget:main"]}
  )
 
---
+++ src/pycistarget/cli/pycistarget.py
@@ -0,0 +1,8 @@
+import sys
+
+def main(argv=None) -> int:
+    print("pycistarget baby!!")
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
