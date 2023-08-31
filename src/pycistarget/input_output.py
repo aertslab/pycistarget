33895b957eea4369ee884737ce153322c772ea83
SeppeDeWinter <seppe.dewinter@hotmail.com>
2023-08-31 15:09:05 +0000

    Make write_hdf5 method of MotifEnrichmentResult and add ability to read/write DEM analysis hdf5 files

--- src/pycistarget/input_output.py
+++ src/pycistarget/input_output.py
@@ -1,157 +1,23 @@
 """Read/write cistarget and DEM results to/from hdf5 files."""
 
 import h5py
-import numpy as np
-from typing import Literal, Dict, List
+from typing import  Dict, List, Union
 
 import pyranges as pr
 from pycistarget.motif_enrichment_cistarget import cisTarget
-import pycistarget
+from pycistarget.motif_enrichment_dem import DEM
 import pandas as pd
 
-_CISTARGET_METADATA_FIELD = [
-    "specie",
-    "auc_threshold",
-    "nes_threshold",
-    "rank_threshold",
-    "annotation_version",
-    "annotation",
-    "path_to_motif_annotations",
-    "motif_similarity_fdr",
-    "orthologous_identity_threshold",
-    "motifs_to_use",
-]
-
-_DTYPE_MAPPING = {
-    "Logo": "S",
-    "Region_set": "S",
-    "Direct_annot": "S",
-    "Motif_similarity_annot": "S",
-    "Orthology_annot": "S",
-    "Motif_similarity_and_Orthology_annot": "S",
-    "NES": np.float64,
-    "AUC": np.float64,
-    "Rank_at_max": np.float64,
-    "Motif_hits": np.int64,
-    "Target": "S",
-    "Query": "S",
-}
-
-
-def write_cistarget(
-    cistarget: cisTarget,
-    path: str,
-    mode: Literal["w", "a"] = "w",
-):
-    """
-    Write cisTarget class to hdf5 file.
-
-    Parameters
-    ----------
-    cistarget: cisTarget
-        cisTarget class to write to disk.
-    path_or_buf: Union[str, h5py.File]
-        A path (str) to which to write the cisTarget class
-        or a hdf5 file buffer.
-    mode: Literal["w", "a"]
-        Mode used to open file.
-        Defaults to 'w'
-    """
-    h5 = h5py.File(path, mode)
-    # Set root to name of cistarget run
-    h5_root = h5.create_group(cistarget.name)
-
-    # Save metadata
-    h5_metadata_grp = h5_root.create_group("metadata")
-    for metadata_field in _CISTARGET_METADATA_FIELD:
-        data = getattr(cistarget, metadata_field)
-        if data is not None:
-            h5_metadata_grp.create_dataset(name=metadata_field, data=data)
-    h5_metadata_grp.create_dataset(name="version", data=pycistarget.__version__)
-
-    # Save cistromes
-    h5_cistromes_grp = h5_root.create_group("cistromes")
-
-    # Save database coordinates of cistromes
-    h5_cistromes_database_grp = h5_cistromes_grp.create_group("database")
-    for cistrome in cistarget.cistromes["Database"].keys():
-        h5_cistromes_database_grp.create_dataset(
-            name=cistrome,
-            data=np.array(cistarget.cistromes["Database"][cistrome], dtype="S"),
-        )
-
-    # Save region_set coordinates of cistromes
-    h5_cistromes_regionset_grp = h5_cistromes_grp.create_group("region_set")
-    for cistrome in cistarget.cistromes["Region_set"].keys():
-        h5_cistromes_regionset_grp.create_dataset(
-            name=cistrome,
-            data=np.array(cistarget.cistromes["Region_set"][cistrome], dtype="S"),
-        )
-
-    # Save motif_hits
-    h5_motifhits_grp = h5_root.create_group("motif_hits")
-
-    # Save database coordinates of motif hits
-    h5_motifhits_database_grp = h5_motifhits_grp.create_group("database")
-    for motif in cistarget.motif_hits["Database"].keys():
-        h5_motifhits_database_grp.create_dataset(
-            name=motif,
-            data=np.array(cistarget.motif_hits["Database"][motif], dtype="S"),
-        )
-
-    # Save region_set coordinates of motif hits
-    h5_motifhits_regionset_grp = h5_motifhits_grp.create_group("region_set")
-    for motif in cistarget.motif_hits["Region_set"].keys():
-        h5_motifhits_regionset_grp.create_dataset(
-            name=motif,
-            data=np.array(cistarget.motif_hits["Region_set"][motif], dtype="S"),
-        )
-
-    # Make copy of motif enrichment dataframe and set data types
-    motif_enrichment = cistarget.motif_enrichment.copy()
-    motif_enrichment = motif_enrichment.astype(
-        {
-            key: _DTYPE_MAPPING[key]
-            for key in _DTYPE_MAPPING.keys()
-            if key in motif_enrichment.columns
-        }
-    )
-
-    # Close file handle.
-    h5.close()
-
-    # Write motif enrichment to disk
-    motif_enrichment.to_hdf(
-        path, key=f"{cistarget.name}/motif_enrichment", append=True, mode="r+"
-    )
-
-    # Make a copy of regions_to_db dataframe
-    # remove index, which is a copy of the target columns
-    # and set data types.
-    regions_to_db = cistarget.regions_to_db.copy()
-    regions_to_db.reset_index(drop=True)
-    regions_to_db = regions_to_db.astype(
-        {
-            key: _DTYPE_MAPPING[key]
-            for key in _DTYPE_MAPPING.keys()
-            if key in regions_to_db.columns
-        }
-    )
-    # Write regions_to_db to disk
-    regions_to_db.to_hdf(
-        path, key=f"{cistarget.name}/regions_to_db", append=True, mode="r+"
-    )
-
-
-def read_cistarget_hdf5(path=str) -> Dict[str, cisTarget]:
+def read_hdf5(path: str) -> Dict[str, Union[cisTarget, DEM]]:
     # Open hdf5 file
     h5 = h5py.File(path, mode="r")
 
-    dict_cistarget: Dict[str, cisTarget] = {}
+    return_dict: Dict[str, Union[cisTarget, DEM]] = {}
 
     # Loop over root keys of hdf5 file.
     # In case multiple cisTarget results are saved in a single file.
     for name in h5.keys():
+        analysis_type = h5[name]["metadata"]["type"][()].decode()
         # Read cistromes
         cistromes: Dict[str, Dict[str, List[str]]] = {}
         for database_or_regionset in h5[name]["cistromes"].keys():
@@ -169,18 +35,38 @@ def read_cistarget_hdf5(path=str) -> Dict[str, cisTarget]:
                 motif_hits[database_or_regionset][motif_name] = list(map(
                             bytes.decode,
                             h5[name]["motif_hits"][database_or_regionset][motif_name][:]))
-
         # Read metadata
-        metadata_kwargs = dict(
-            annotation = map(bytes.decode, h5[name]["metadata"]["annotation"][()]),
-            annotation_version = h5[name]["metadata"]["annotation_version"][()].decode(),
-            auc_threshold = h5[name]["metadata"]["auc_threshold"][()],
-            motif_similarity_fdr = h5[name]["metadata"]["motif_similarity_fdr"][()],
-            nes_threshold = h5[name]["metadata"]["nes_threshold"][()],
-            orthologous_identity_threshold =  h5[name]["metadata"]["orthologous_identity_threshold"][()],
-            rank_threshold = h5[name]["metadata"]["rank_threshold"][()],
-            specie = h5[name]["metadata"]["specie"][()],
-        )
+        if analysis_type == "cisTarget":
+            metadata_kwargs = dict(
+                annotation_to_use = map(bytes.decode, h5[name]["metadata"]["annotation_to_use"][()]),
+                annotation_version = h5[name]["metadata"]["annotation_version"][()].decode(),
+                auc_threshold = h5[name]["metadata"]["auc_threshold"][()],
+                motif_similarity_fdr = h5[name]["metadata"]["motif_similarity_fdr"][()],
+                nes_threshold = h5[name]["metadata"]["nes_threshold"][()],
+                orthologous_identity_threshold =  h5[name]["metadata"]["orthologous_identity_threshold"][()],
+                rank_threshold = h5[name]["metadata"]["rank_threshold"][()],
+                species = h5[name]["metadata"]["species"][()],
+            )
+        elif analysis_type == "DEM":
+            metadata_kwargs = dict(
+                annotation_to_use = map(bytes.decode, h5[name]["metadata"]["annotation_to_use"][()]),
+                annotation_version = h5[name]["metadata"]["annotation_version"][()].decode(),
+                log2fc_thr = h5[name]["metadata"]["log2fc_thr"][()],
+                adjpval_thr = h5[name]["metadata"]["adjpval_thr"][()],
+                motif_hit_thr = h5[name]["metadata"]["motif_hit_thr"][()],
+                motif_similarity_fdr = h5[name]["metadata"]["motif_similarity_fdr"][()],
+                mean_fg_thr = h5[name]["metadata"]["mean_fg_thr"][()],
+                orthologous_identity_threshold =  h5[name]["metadata"]["orthologous_identity_threshold"][()],
+                species = h5[name]["metadata"]["species"][()],
+            )
+        else:
+            raise ValueError(f"Unrecognized type {analysis_type}")
+
+        #optional arguments
+        if "motifs_to_use" in h5[name]["metadata"].keys():
+            metadata_kwargs["motifs_to_use"] = map(bytes.decode, h5[name]["metadata"]["motifs_to_use"][()])
+        if "path_to_motif_annotations" in h5[name]["metadata"].keys():
+            metadata_kwargs["path_to_motif_annotations"] = h5[name]["metadata"]["path_to_motif_annotations"][()].decode()
 
         # Read motif enrichment and regions_to_db
         motif_enrichment = pd.read_hdf(
@@ -191,17 +77,21 @@ def read_cistarget_hdf5(path=str) -> Dict[str, cisTarget]:
             path, key = f"{name}/regions_to_db"
         )
 
-        dict_cistarget[name] = cisTarget(
-            region_set=pr.PyRanges(), # Empty pyranges
-            name = name,
-            **metadata_kwargs)
-        dict_cistarget[name].cistromes = cistromes
-        dict_cistarget[name].motif_hits = motif_hits
-        dict_cistarget[name].motif_enrichment = motif_enrichment
-        dict_cistarget[name].regions_to_db = regions_to_db
+        if analysis_type == "cisTarget":
+            return_dict[name] = cisTarget(
+                region_set=pr.PyRanges(), # Empty pyranges
+                name = name,
+                **metadata_kwargs)
+        elif analysis_type == "DEM":
+            return_dict[name] = DEM(
+                foreground_regions=pr.PyRanges(), # Empty pyranges
+                background_regions=pr.PyRanges(), # Empty pyranges TODO: save foreground and background regions?
+                name = name,
+                **metadata_kwargs)
+        return_dict[name].cistromes = cistromes
+        return_dict[name].motif_hits = motif_hits
+        return_dict[name].motif_enrichment = motif_enrichment
+        return_dict[name].regions_to_db = regions_to_db
 
     h5.close()
-
-
-
-    return dict_cistarget
+    return return_dict
--- src/pycistarget/motif_enrichment_result.py
+++ src/pycistarget/motif_enrichment_result.py
@@ -1,10 +1,62 @@
+import pycistarget
 from pycistarget.utils import load_motif_annotations, get_cistromes_per_region_set
+from pycistarget.motif_enrichment_cistarget import cisTarget
+from pycistarget.motif_enrichment_dem import DEM
 from typing import Optional, List, Dict, Literal
+import h5py
+import numpy as np
 import pandas as pd
 import ssl
 from IPython.display import HTML
 ssl._create_default_https_context = ssl._create_unverified_context
 pd.set_option('display.max_colwidth', None)
+
+_METADATA_FIELDS = {
+    "cisTarget": [
+        "species",
+        "auc_threshold",
+        "nes_threshold",
+        "rank_threshold",
+        "annotation_version",
+        "annotation_to_use",
+        "path_to_motif_annotations",
+        "motif_similarity_fdr",
+        "orthologous_identity_threshold",
+        "motifs_to_use"
+    ],
+    "DEM": [
+        "species",
+        "adjpval_thr",
+        "log2fc_thr",
+        "mean_fg_thr",
+        "motif_hit_thr",
+        "path_to_motif_annotations",
+        "annotation_version",
+        "annotation_to_use",
+        "motif_similarity_fdr",
+        "orthologous_identity_threshold",
+        "motifs_to_use"
+    ]
+}
+
+_DTYPE_MAPPING = {
+    "Logo": "S",
+    "Region_set": "S",
+    "Direct_annot": "S",
+    "Motif_similarity_annot": "S",
+    "Orthology_annot": "S",
+    "Motif_similarity_and_Orthology_annot": "S",
+    "NES": np.float64,
+    "AUC": np.float64,
+    "Rank_at_max": np.float64,
+    "Motif_hits": np.int64,
+    "Target": "S",
+    "Query": "S",
+    "Log2FC": np.float64,
+    "Adjusted_pval": np.float64,
+    "Mean_fg": np.float64,
+    "Mean_bg": np.float64
+}
 
 class MotifEnrichmentResult:
     def __init__(
@@ -33,6 +85,7 @@ class MotifEnrichmentResult:
             "Database": dict(), "Region_set": dict()}
         self.cistromes: Dict[str, Dict[str, List[str]]] = {
             "Database": dict(), "Region_set": dict()}
+        self.regions_to_db = pd.DataFrame()
 
     def add_motif_annotation(
             self,
@@ -76,3 +129,100 @@ class MotifEnrichmentResult:
     
     def show_result(self):
         return HTML(self.motif_enrichment.to_html(escape = False, col_space = 80))
+    
+    def write_hdf5(
+            self,
+            path: str,
+            mode: Literal["w", "a"] = "w"):
+        # Get analysis type
+        if isinstance(self, DEM):
+            analysis_type = "DEM"
+        elif isinstance(self, cisTarget):
+            analysis_type = "cisTarget"
+        else:
+            raise ValueError(f"Unrecognized analysis type: {type(self)}")
+        h5 = h5py.File(path, mode)
+        # Set root to name of cistarget run
+        h5_root = h5.create_group(self.name)
+
+        # Save metadata
+        h5_metadata_grp = h5_root.create_group("metadata")
+        for metadata_field in _METADATA_FIELDS[analysis_type]:
+            data = getattr(self, metadata_field)
+            if data is not None:
+                h5_metadata_grp.create_dataset(name=metadata_field, data=data)
+        h5_metadata_grp.create_dataset(name="version", data=pycistarget.__version__)
+        h5_metadata_grp.create_dataset(name="type", data=analysis_type)
+
+        # Save cistromes
+        h5_cistromes_grp = h5_root.create_group("cistromes")
+
+        # Save database coordinates of cistromes
+        h5_cistromes_database_grp = h5_cistromes_grp.create_group("database")
+        for cistrome in self.cistromes["Database"].keys():
+            h5_cistromes_database_grp.create_dataset(
+                name=cistrome,
+                data=np.array(self.cistromes["Database"][cistrome], dtype="S"),
+            )
+
+        # Save region_set coordinates of cistromes
+        h5_cistromes_regionset_grp = h5_cistromes_grp.create_group("region_set")
+        for cistrome in self.cistromes["Region_set"].keys():
+            h5_cistromes_regionset_grp.create_dataset(
+                name=cistrome,
+                data=np.array(self.cistromes["Region_set"][cistrome], dtype="S"),
+            )
+
+        # Save motif_hits
+        h5_motifhits_grp = h5_root.create_group("motif_hits")
+
+        # Save database coordinates of motif hits
+        h5_motifhits_database_grp = h5_motifhits_grp.create_group("database")
+        for motif in self.motif_hits["Database"].keys():
+            h5_motifhits_database_grp.create_dataset(
+                name=motif,
+                data=np.array(self.motif_hits["Database"][motif], dtype="S"),
+            )
+
+        # Save region_set coordinates of motif hits
+        h5_motifhits_regionset_grp = h5_motifhits_grp.create_group("region_set")
+        for motif in self.motif_hits["Region_set"].keys():
+            h5_motifhits_regionset_grp.create_dataset(
+                name=motif,
+                data=np.array(self.motif_hits["Region_set"][motif], dtype="S"),
+            )
+
+        # Make copy of motif enrichment dataframe and set data types
+        motif_enrichment = self.motif_enrichment.copy()
+        motif_enrichment = motif_enrichment.astype(
+            {
+                key: _DTYPE_MAPPING[key]
+                for key in _DTYPE_MAPPING.keys()
+                if key in motif_enrichment.columns
+            }
+        )
+
+        # Close file handle.
+        h5.close()
+
+        # Write motif enrichment to disk
+        motif_enrichment.to_hdf(
+            path, key=f"{self.name}/motif_enrichment", append=True, mode="r+"
+        )
+
+        # Make a copy of regions_to_db dataframe
+        # remove index, which is a copy of the target columns
+        # and set data types.
+        regions_to_db = self.regions_to_db.copy()
+        regions_to_db.reset_index(drop=True)
+        regions_to_db = regions_to_db.astype(
+            {
+                key: _DTYPE_MAPPING[key]
+                for key in _DTYPE_MAPPING.keys()
+                if key in regions_to_db.columns
+            }
+        )
+        # Write regions_to_db to disk
+        regions_to_db.to_hdf(
+            path, key=f"{self.name}/regions_to_db", append=True, mode="r+"
+        )
